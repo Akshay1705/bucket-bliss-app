7ca128cd6972f1eddd060b08b3802ce916f7cc40
Akshay1705 <akstyle1705@gmail.com>
2025-07-07 21:01:33 +0530

    Day 2: Add POST /api/wishes route with validation and memory storage

--- index.js
+++ index.js
@@ -2,18 +2,43 @@
 // import express, { json } from 'express'; <--also this vaild 
 const express = require('express');
 const app = express();//Initializes Express server
-const PORT = 5000;
+const PORT = 4000;
 
 // Middleware to parse JSON
 app.use(express.json());//Lets the server understand JSON bodies
 
+let wishes = []; // temporary data store - It’s just an empty array in memory
+
 // Sample route
 app.get('/api/wishes', (req, res) => {//Defines a route that responds to GET requests
   res.json({ message: 'Welcome to Bucket Bliss – Wishlist API!' });//Sends back a JSON response
+  // res.json(wishes);//Sends back a JSON response
 });
 
-// Start server
-app.listen(PORT, () => {//Starts the server on a specific port
+app.post('/api/wishes', (req, res) => {
+  const { title, category, targetDate } = req.body;
+
+  // Validate input
+  if (!title || !category || !targetDate) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  const newWish = {
+    id: Date.now(), // Temporary ID
+    title,
+    category,
+    targetDate,
+    isCompleted: false
+  };
+
+  wishes.push(newWish);//acting like adding a row in a database.
+
+  res.status(201).json({ message: 'Wish added successfully', wish: newWish });
+});
+
+
+//Starts the server on a specific port
+app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
 
