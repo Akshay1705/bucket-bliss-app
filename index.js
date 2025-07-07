c59d4a4f44eb8839e3e01cc612d87e43635f67ad
Akshay1705 <akstyle1705@gmail.com>
2025-07-07 21:39:27 +0530

    Day 3: Add filtering to GET /api/wishes using query params

--- index.js
+++ index.js
@@ -10,10 +10,33 @@ app.use(express.json());//Lets the server understand JSON bodies
 let wishes = []; // temporary data store - It’s just an empty array in memory
 
 // Sample route
-app.get('/api/wishes', (req, res) => {//Defines a route that responds to GET requests
-  res.json({ message: 'Welcome to Bucket Bliss – Wishlist API!' });//Sends back a JSON response
-  // res.json(wishes);//Sends back a JSON response
+app.get('/api/wishes', (req, res) => {
+  const { category, status } = req.query;
+
+  if (wishes[1]) {
+    wishes[1].isCompleted = true;
+  }
+  
+  let filteredWishes = wishes;
+
+  // Filter by category (if given)
+  if (category) {
+    filteredWishes = filteredWishes.filter(
+      (wish) => wish.category.toLowerCase() === category.toLowerCase()
+    );
+  }
+
+  // Filter by status (if given)
+  if (status) {
+    const isCompleted = status.toLowerCase() === 'completed';
+    filteredWishes = filteredWishes.filter(
+      (wish) => wish.isCompleted === isCompleted
+    );
+  }
+
+  res.json(filteredWishes);
 });
+
 
 app.post('/api/wishes', (req, res) => {
   const { title, category, targetDate } = req.body;
